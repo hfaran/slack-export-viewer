2c992597f29161c3a75580cf0a0eef9c00cd01c2
Tad Scritchfield <tadscritch@gmail.com>
2018-02-15 12:20:40 -0700

    add dms to every page

--- slackviewer/app.py
+++ slackviewer/app.py
@@ -13,24 +13,26 @@ def channel_name(name):
     messages = flask._app_ctx_stack.channels[name]
     channels = list(flask._app_ctx_stack.channels.keys())
     groups = list(flask._app_ctx_stack.groups.keys())
-    dms = list(flask._app_ctx_stack.dms.keys())
+    dm_users = list(flask._app_ctx_stack.dm_users)
+
     return flask.render_template("viewer.html", messages=messages,
                                  name=name.format(name=name),
                                  channels=sorted(channels),
                                  groups=sorted(groups),
-                                 dms=sorted(dms))
+                                 dm_users=sorted(dm_users))
 
 @app.route("/group/<name>/")
 def group_name(name):
     messages = flask._app_ctx_stack.groups[name]
     channels = list(flask._app_ctx_stack.channels.keys())
     groups = list(flask._app_ctx_stack.groups.keys())
-    dms = list(flask._app_ctx_stack.dms.keys())
+    dm_users = list(flask._app_ctx_stack.dm_users)
+
     return flask.render_template("viewer.html", messages=messages,
                                  name=name.format(name=name),
                                  channels=sorted(channels),
                                  groups=sorted(groups),
-                                 dms=sorted(dms))
+                                 dm_users=sorted(dm_users))
 
 @app.route("/dm/<id>/")
 def dm_id(id):
