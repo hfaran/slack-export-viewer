9ced50f8a93f890e7eaff880642591b29d760b62
Hamza Faran <hamzafaran@outlook.com>
2018-03-18 01:43:27 -0700

    Small fixes to trigger travis build

--- slackviewer/main.py
+++ slackviewer/main.py
@@ -78,7 +78,9 @@ def configure_app(app, archive, debug):
               default=flag_ennvar("SEV_NO_BROWSER"),
               help="If you do not want a browser to open "
                    "automatically, set this.")
-@click.option('--test', is_flag=True, default=flag_ennvar("SEV_TEST"), help="Runs in 'test' mode, i.e., this will do an archive extract, but will not start the server, and immediately quit.")
+@click.option('--test', is_flag=True, default=flag_ennvar("SEV_TEST"),
+              help="Runs in 'test' mode, i.e., this will do an archive extract, but will not start the server,"
+                   " and immediately quit.")
 @click.option('--debug', is_flag=True, default=flag_ennvar("FLASK_DEBUG"))
 def main(port, archive, ip, no_browser, test, debug):
     if not archive:
