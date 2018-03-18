346652f94bb8570c68ce47a629ad7a3d80153c8a
Hamza Faran <hamzafaran@outlook.com>
2018-03-18 01:20:34 -0700

    Add test flag and travis yml

--- slackviewer/main.py
+++ slackviewer/main.py
@@ -78,17 +78,19 @@ def configure_app(app, archive, debug):
               default=flag_ennvar("SEV_NO_BROWSER"),
               help="If you do not want a browser to open "
                    "automatically, set this.")
+@click.option('--test', is_flag=True, default=flag_ennvar("SEV_TEST"), help="Runs in 'test' mode, i.e., this will do an archive extract, but will not start the server, and immediately quit.")
 @click.option('--debug', is_flag=True, default=flag_ennvar("FLASK_DEBUG"))
-def main(port, archive, ip, no_browser, debug):
+def main(port, archive, ip, no_browser, test, debug):
     if not archive:
         raise ValueError("Empty path provided for archive")
 
     configure_app(app, archive, debug)
 
-    if not no_browser:
+    if not no_browser and not test:
         webbrowser.open("http://{}:{}".format(ip, port))
 
-    app.run(
-        host=ip,
-        port=port
-    )
+    if not test:
+        app.run(
+            host=ip,
+            port=port
+        )
