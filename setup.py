30be9c9fd5fdd3db779312fdb71b91379a8d454b
hfaran <hamzafaran@outlook.com>
2019-07-24 21:21:55 -0700

    Release 0.9.4

--- setup.py
+++ setup.py
@@ -13,12 +13,7 @@ def read(filename):
 
 
 install_requires = read("requirements.txt").split()
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-    long_description = long_description.replace("\r","")
-except(IOError, ImportError):
-    long_description = read('README.md')
+long_description = read('README.md')
 
 
 setup(
