6baeec5244e0b176ac48346072ea8afcb3e5ffb4
Josh Owen <josh@openfolio.com>
2020-05-27 15:04:40 -0400

    Handle missing bot_id

--- slackviewer/formatter.py
+++ slackviewer/formatter.py
@@ -29,7 +29,7 @@ class SlackFormatter(object):
     def find_user(self, message):
         if message.get("user") == "USLACKBOT":
             return User({"name":"slackbot"})
-        if message.get("subtype", "").startswith("bot_") and message["bot_id"] not in self.__USER_DATA:
+        if message.get("subtype", "").startswith("bot_") and "bot_id" in message and message["bot_id"] not in self.__USER_DATA:
             bot_id = message["bot_id"]
             logging.debug("bot addition for %s", bot_id)
             if "bot_link" in message:
